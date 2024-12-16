3daa7d7ac85d1053efcb22a09d02c883d760e16d
Sollimba <134264168+Sollimba@users.noreply.github.com>
2024-12-16 04:19:30 +0300

    Kyrs6

--- KyrsovProect/inventoryproject/dashboard/models.py
+++ KyrsovProect/inventoryproject/dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY = (
+    ('Stationary','Stationary'),
+    ('Electronics','Electronics'),
+    ('Smartphone','Smartphone')
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveBigIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.category}-{self.name}-{self.quantity}'
--- KyrsovProect/inventoryproject/dashboard/views.py
+++ KyrsovProect/inventoryproject/dashboard/views.py
@@ -6,6 +6,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'dashboard/index.html')
 
-
 def staff(request):
     return render(request, 'dashboard/staff.html')
+
+def product(request):
+    return render(request, 'dashboard/product.html')
+
+def order(request):
+    return render(request, 'dashboard/order.html')
