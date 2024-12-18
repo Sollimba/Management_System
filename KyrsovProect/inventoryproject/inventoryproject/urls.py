64546d1efeb80861be34b3e7d09543180001cfbf
Sollimba <134264168+Sollimba@users.noreply.github.com>
2024-12-18 15:41:26 +0300

    Kyrs11.4__logout

--- KyrsovProect/inventoryproject/inventoryproject/urls.py
+++ KyrsovProect/inventoryproject/inventoryproject/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from user import views as user_view
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_view.register, name='user-register'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name = 'user-login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/login.html'), name='user-logout'),
+    path('logout/', auth_views.LoginView.as_view(template_name='user/logout.html'), name='user-logout'),
+
 ]
